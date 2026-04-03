50a35fec04c5c29854775144c182c27453838f0e
Raymond Hicks <arpie71@gmail.com>
2026-04-03 08:15:28 -0400

    More changes 2

--- gifts_stream.py
+++ gifts_stream.py
@@ -166,6 +166,8 @@ if not remaining_indices or st.session_state.idx > len(remaining_indices):
 current_original_idx = remaining_indices[st.session_state.idx]
 current_row = gifts_df.loc[current_original_idx].astype(str)
 
+st.write(current_row)
+
 # Only preload when index changes
 if st.session_state.last_loaded_idx != current_original_idx:
     if current_original_idx in st.session_state.edits:
