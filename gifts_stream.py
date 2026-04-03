60844d6575443289bf88d8c6206501c1f6d9d7e4
Raymond Hicks <arpie71@gmail.com>
2026-04-03 08:30:57 -0400

    Fixed WikidataID dtype errors

--- gifts_stream.py
+++ gifts_stream.py
@@ -163,7 +163,6 @@ if not remaining_indices or st.session_state.idx > len(remaining_indices):
 
 current_original_idx = remaining_indices[st.session_state.idx]
 current_row = gifts_df.loc[current_original_idx].astype(str)
-st.write(current_row)
 
 # Only preload when index changes
 if st.session_state.last_loaded_idx != current_original_idx:
